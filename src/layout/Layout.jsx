666090ed0c29cb82541bb93c1ab0ce15ebf7fde1
eeheg <sjh8924@naver.com>
2023-12-08 18:48:45 +0900

    feat: 웹소켓연결

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,125 +1,77 @@
-import { Link, Outlet } from 'react-router-dom';
-import styles from './Layout.module.css';
+import { Link, Outlet } from "react-router-dom";
+import styles from "./Layout.module.css";
+import { useSocket } from "@/hooks/useSocket";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const { doOpen } = useSocket();
+
+  useEffect(() => {
+    doOpen();
+  }, []);
+
   return (
     <section className={styles.wrapper}>
       <header className={styles.header}>
         <h2 className={styles.title}>
-          <img
-            src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/logo.svg`}
-            alt='로고'
-          />
+          <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/logo.svg`} alt="로고" />
           메가존클라우드
         </h2>
         <div className={styles.headerSearchWrapper}>
-          <img
-            src={`${
-              import.meta.env.VITE_CLOUD_FRONT_ID
-            }/free-icon-font-search-3917754 1.svg`}
-            alt='검색'
-          />
-          <input
-            type='text'
-            placeholder='교육생 검색'
-            className={styles.headerSearch}
-          />
+          <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-search-3917754 1.svg`} alt="검색" />
+          <input type="text" placeholder="교육생 검색" className={styles.headerSearch} />
         </div>
         <div>
-          <img
-            src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/profile.svg`}
-            alt='프로필'
-          />
+          <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/profile.svg`} alt="프로필" />
         </div>
       </header>
       <section className={styles.contentWrapper}>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
             <li className={styles.navItem}>
-              <Link to='/' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-home-3917033 1.svg`}
-                  alt='홈'
-                />
-                홈
+              <Link to="/" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-home-3917033 1.svg`} alt="홈" />홈
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/notice' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-megaphone-3914404 1.svg`}
-                  alt='공지사항'
-                />
+              <Link to="/notice" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-megaphone-3914404 1.svg`} alt="공지사항" />
                 공지사항
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/curriculum' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-clipboard-list-7857307 1.svg`}
-                  alt='커리큘럼'
-                />
+              <Link to="/curriculum" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-clipboard-list-7857307 1.svg`} alt="커리큘럼" />
                 커리큘럼
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/student' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-users-3914353 1.svg`}
-                  alt='교육생'
-                />
+              <Link to="/student" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-users-3914353 1.svg`} alt="교육생" />
                 교육생
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/attendance' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-user-time-3914150 1.svg`}
-                  alt='출결'
-                />
+              <Link to="/attendance" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-user-time-3914150 1.svg`} alt="출결" />
                 출결
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/qr' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-qr-scan-12436470 1.svg`}
-                  alt='QR'
-                />
+              <Link to="/qr" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-qr-scan-12436470 1.svg`} alt="QR" />
                 QR
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/note' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-paper-plane-3917567 2.svg`}
-                  alt='쪽지'
-                />
+              <Link to="/note" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-paper-plane-3917567 2.svg`} alt="쪽지" />
                 쪽지
               </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to='/setting' className={styles.navLink}>
-                <img
-                  src={`${
-                    import.meta.env.VITE_CLOUD_FRONT_ID
-                  }/free-icon-font-settings-3917058 1.svg`}
-                  alt='설정'
-                />
+              <Link to="/setting" className={styles.navLink}>
+                <img src={`${import.meta.env.VITE_CLOUD_FRONT_ID}/free-icon-font-settings-3917058 1.svg`} alt="설정" />
                 설정
               </Link>
             </li>
