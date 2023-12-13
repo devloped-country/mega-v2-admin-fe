d1214f07ef232177a47e912c540006af8c737ed0
ubeom kim <kimub1204@gmail.com>
2023-12-13 22:21:40 +0900

    feat: 커리큘럼 crud 기능 추가

--- src/components/Curriculum/CurriculumItem.jsx
+++ src/components/Curriculum/CurriculumItem.jsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
-import styles from './CurriculumItem.module.css'
-import { Navigate, useFetcher, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import styles from './CurriculumItem.module.css';
 import { useMenuBlur } from '@/hooks/useMenuBlur';
-import CurriculumUpdateModal from './CurriculumUpdateModal';
-import CurriculumDeleteModal from "./CurriculumDeleteModal";
-import { useFetch } from '@/hooks/useFetch';
-import axios from 'axios';
 
-function CurriculumItem({ id, subject, time, curriculumId, startDate, endDate, contents, onClick }) {
+function CurriculumItem({
+  id,
+  subject,
+  courseId,
+  time,
+  curriculumId,
+  startDate,
+  endDate,
+  contents,
+  onClick,
+  setIsShowingDeleteModal,
+  setIsShowingUpdateModal,
+}) {
   const [isShowingMenu, setIsShowingMenu] = useState(false);
-  const [isShowingUpdateModal, setIsShowingUpdateModal] = useState(false);
-  const [isShowingDeleteModal, setIsShowingDeleteModal] = useState(false);
-  const navigate = useNavigate();
 
   const start = new Date(startDate);
   const end = new Date(endDate);
   const curDate = Date.now();
 
-  const clickeButton = () => {
-    setIsShowingMenu(prev => !prev);
-    navigate(`/curriculum/${id}`, {
-      state: { id, subject, time, startDate, endDate, contents },
-    });
-  };
-
-  const menuBlurCallback = ({target}) => {
+  const menuBlurCallback = ({ target }) => {
     if (target.dataset.tag !== 'curriculumMenu' && isShowingMenu) {
       setIsShowingMenu(false);
     }
-  }
+  };
 
   useMenuBlur({ dep: [isShowingMenu], callback: menuBlurCallback });
 
@@ -43,153 +40,75 @@ function CurriculumItem({ id, subject, time, curriculumId, startDate, endDate, c
     setIsShowingMenu(true);
   };
 
-  const handleClickUpdateButton = () => {
-    setIsShowingUpdateModal(true);
-    setIsShowingMenu(false);
-  };
-
-  const closeUpdateModal = () => {
-    setIsShowingUpdateModal(false);
-  }
-
-
   const handleClickDeleteButton = () => {
     setIsShowingDeleteModal(true);
     setIsShowingMenu(false);
     onClick(id);
-  }
+  };
 
-  const closeDeleteModal = () => {
-    setIsShowingDeleteModal(false);
-  }
+  const handleClickUpdateButton = () => {
+    setIsShowingUpdateModal(true);
+    setIsShowingMenu(false);
+    onClick(id);
+  };
 
-
-  const mapedContent = contents.map(
-    ({content}, index) => {
-      return (
-        <div key={index}
-        > {content}  /</div>
-      )
-    }
-  )
-
-
-  id = 2;
-  const {
-    data: curriculum,
-    isLoading
-  } = useFetch(
-    [],
-    async () => await axios(`/api/curriculum/read/${id}`)
-  );
-
-  if(isLoading) {
-    return 
-  }
-  
-  const mapedUpdateSubject = curriculum.data.data.map(
-    ({curriculum_id, subject, time, startDate, endDate, content}) => {
-      return (
-        <CurriculumUpdateModal 
-          key={curriculum_id}
-          id={curriculum_id}
-          subject={subject}
-          time={time}
-          startDate={startDate}
-          endDate={endDate}
-          contents={content}
-          title1='기본 정보'
-          title2='상세 정보'
-          onClose={closeUpdateModal}
-        />
-      )
-    }
-  )
-
-  const mapedDeleteSubject = curriculum.data.data.map(
-    ({curriculum_id, subject, time, startDate, endDate, content}) => {
-      return (
-        <CurriculumDeleteModal 
-          key={curriculum_id}
-          id={curriculum_id}
-          curriculumId={curriculumId}
-          subject={subject}
-          time={time}
-          startDate={startDate}
-          endDate={endDate}
-          contents={content}
-          title1='기본 정보'
-          title2='상세 정보'
-          onClose={closeDeleteModal}
-        />
-      )
-    }
-  )
-  
+  const mapedContent = contents.map(({ content }, index) => {
+    return <div key={index}>&nbsp;{content}&nbsp;/</div>;
+  });
 
   return (
-    <>
-      <li className={styles.item}>
-        <header className={styles.header}>
-          <div className={styles.textAlign}>
-            {start - curDate < 0 && end - curDate > 0 ? (
-              <img src='https://d2f3kqq80r3o3g.cloudfront.net/Frame%20303.svg' />
-            ) : (
-              <img src='https://d2f3kqq80r3o3g.cloudfront.net/Frame%20304.svg' />
-            )}
-            <h3 className={styles.title}>{subject}</h3>
-            <h3 className={styles.timePosition}>{time}</h3>
-          </div>
+    <li className={styles.item}>
+      <header className={styles.header}>
+        <div className={styles.textAlign}>
+          {start - curDate < 0 && end - curDate > 0 ? (
+            <img src='https://d2f3kqq80r3o3g.cloudfront.net/Frame%20303.svg' />
+          ) : (
+            <img src='https://d2f3kqq80r3o3g.cloudfront.net/Frame%20304.svg' />
+          )}
+          <h3 className={styles.title}>{subject}</h3>
+          <h3 className={styles.timePosition}>{time}</h3>
+        </div>
 
-          <button
-            type='button'
-            className={styles.button}
-            onClick={handleClickMenuButton}
+        <button
+          type='button'
+          className={styles.button}
+          onClick={handleClickMenuButton}
+          data-tag='curriculumMenu'
+        >
+          <img
+            src={`${
+              import.meta.env.VITE_CLOUD_FRONT_ID
+            }/free-icon-font-menu-dots-vertical-3917158+1.svg`}
             data-tag='curriculumMenu'
-          >
-            <img
-              src={`${
-                import.meta.env.VITE_CLOUD_FRONT_ID
-              }/free-icon-font-menu-dots-vertical-3917158+1.svg`}
-              data-tag='curriculumMenu'
-              alt='버튼'
-            />
-            {isShowingMenu && (
-              <ul className={styles.menuList} data-tag='curriculumMenu'>
-                <li
-                  className={styles.menuItem}
-                  data-tag='curriculumMenu'
-                  onClick={handleClickUpdateButton}
-                >
-                  수정
-                </li>
-                <li
-                  className={styles.menuItem}
-                  data-tag='noticeMenu'
-                  onClick={handleClickDeleteButton}
-                >
-                  삭제
-                </li>
-              </ul>
-            )}
-          </button>
-        </header>
-        <ul className={styles.info}>
-          <li>
-            {startDate} ~ {endDate}
-          </li>
-        </ul>
-        <p className={styles.content}>{mapedContent}</p>
-      </li>
-      {isShowingUpdateModal && (
-        <CurriculumUpdateModal 
-          title1='기본 정보 입력'
-          title2='상세 정보 입력'
-          onClose={closeUpdateModal}
-        />
-        )
-      }
-    </>
+            alt='버튼'
+          />
+          {isShowingMenu && (
+            <ul className={styles.menuList} data-tag='curriculumMenu'>
+              <li
+                className={styles.menuItem}
+                data-tag='curriculumMenu'
+                onClick={handleClickUpdateButton}
+              >
+                수정
+              </li>
+              <li
+                className={styles.menuItem}
+                data-tag='noticeMenu'
+                onClick={handleClickDeleteButton}
+              >
+                삭제
+              </li>
+            </ul>
+          )}
+        </button>
+      </header>
+      <ul className={styles.info}>
+        <li>
+          {startDate} ~ {endDate}
+        </li>
+      </ul>
+      <div className={styles.content}>{mapedContent}</div>
+    </li>
   );
 }
 
--- src/pages/curriculum/Curriculum.jsx
+++ src/pages/curriculum/Curriculum.jsx
@@ -3,17 +3,39 @@ import ContentHeader2 from '@components/common/ContentHeader2';
 import CurriculumContent from '@components/Curriculum/CurriculumContent';
 import { useNavigate } from 'react-router-dom';
 import CurriculumAddModals from '../../components/Curriculum/CurriculumAddModals';
-
+import { useFetch } from '@/hooks/useFetch';
+import axios from 'axios';
 
 function Curriculum() {
   const navigate = useNavigate();
-  const [classes, setClasses] = useState([
-    { name: '클라우드 네이티브 애플리케이션 개발자 양성과정', class: 'dev' },
-    { name: '클라우드 엔지니어 전문가 양성과정', class: 'devops' },
-  ]);
-
   const [isShowingAddModal, setIsShowingAddModal] = useState(false);
-  
+  const { data, isLoading } = useFetch(
+    [],
+    async () =>
+      await axios({
+        url: '/api/auth/read/manager_course',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }),
+    {
+      onSuccess: ({ data }) => {
+        setCourseId(parseInt(Object.entries(data.courseInfo)[0][0]));
+      },
+    }
+  );
+  const [courseId, setCourseId] = useState(
+    data && Object.entries(data.data.courseInfo)[0]
+  );
+  // const [classes, setClasses] = useState([
+  //   { name: '클라우드 네이티브 애플리케이션 개발자 양성과정', class: 'dev' },
+  //   { name: '클라우드 엔지니어 전문가 양성과정', class: 'devops' },
+  // ]);
+
+  if (isLoading) {
+    return;
+  }
+
   const handleClickAddButton = (e) => {
     e.stopPropagation();
     setIsShowingAddModal(true);
@@ -21,18 +43,18 @@ function Curriculum() {
 
   const closeAddModal = () => {
     setIsShowingAddModal(false);
-  }
+  };
 
   const handleClickChangeButton = () => {
     navigate('/curriculum/listChange');
-  }
-
+  };
 
   return (
     <section>
       <ContentHeader2
         title='커리큘럼'
-        classes={classes}
+        classes={Object.entries(data.data.courseInfo)}
+        setCourseId={setCourseId}
         img='https://d2f3kqq80r3o3g.cloudfront.net/free-icon-font-clipboard-list-7857307 1.svg'
         buttonImg='https://d2f3kqq80r3o3g.cloudfront.net/free-icon-font-plus-small-3917179+1.svg'
         buttonText='추가하기'
@@ -42,15 +64,15 @@ function Curriculum() {
         onButtonAction1={handleClickAddButton}
         onButtonAction2={handleClickChangeButton}
       />
-      <CurriculumContent />
+      <CurriculumContent courseId={courseId} />
       {isShowingAddModal && (
         <CurriculumAddModals
+          courseId={courseId}
           title1='기본 정보 입력'
           title2='상세 정보 입력'
           onClose={closeAddModal}
-        /> 
+        />
       )}
-      
     </section>
   );
 }
