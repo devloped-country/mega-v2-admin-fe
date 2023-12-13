e865c10e687fad2b3e0349e997da5de8cfebe4c3
CDDcookie <wlsdnxwlsdn@gamil.com>
2023-12-14 00:24:46 +0900

    feat: 결제기능

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/router/router';
-import { Reset } from 'styled-reset';
-import './main.css';
-import './assets/fonts/font.css';
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/router/router";
+import { Reset } from "styled-reset";
+import "./main.css";
+// import "./App.css";
+import "./assets/fonts/font.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
     <Reset />
     <RouterProvider router={router} />
-  </>
+  </QueryClientProvider>
 );
